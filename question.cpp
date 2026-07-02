0516891a0250d634d9f8f51b5cd49437b5ea88aa
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-07-02 17:34:49 +0530

    Add files via upload

--- question.cpp
+++ question.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Solution {
 public:
     int countDigit(int n) {
@@ -11,3 +12,32 @@ public:
         return count;
     }
 };
+
+
+class Solution {
+public:
+    int reverseNumber(int n) {
+        int rev=0;
+        while(n>0){
+            int digit=n%10;
+            rev=rev*10+digit;
+            n=n/10;
+        }
+        return rev;
+    }
+};
+=======
+class Solution {
+public:
+    int countDigit(int n) {
+        if(n==0)
+        return 1;
+        int count=0;
+        while(n>0){
+            count++;
+            n=n/10;
+        }
+        return count;
+    }
+};
+>>>>>>> 8027f7a6f0f78c5a320d1139afcf4737ddcfa4f1
