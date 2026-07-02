7bfdf80b00e3cda512a822d88ecd5bdbec739227
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-07-02 17:37:40 +0530

    Add files via upload

--- question.cpp
+++ question.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Solution {
 public:
     int countDigit(int n) {
@@ -26,18 +25,21 @@ public:
         return rev;
     }
 };
-=======
+
+
+
 class Solution {
 public:
-    int countDigit(int n) {
-        if(n==0)
-        return 1;
-        int count=0;
-        while(n>0){
-            count++;
-            n=n/10;
+    bool isPalindrome(int n) {
+ int original = n;
+        int rev = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            rev = rev * 10 + digit;
+            n = n / 10;
         }
-        return count;
+
+        return original == rev;
     }
 };
->>>>>>> 8027f7a6f0f78c5a320d1139afcf4737ddcfa4f1
