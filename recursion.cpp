02c95cc35f0223dee6d30568230fc5eb9b95be20
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-07-05 14:28:36 +0530

    Add files via upload

--- recursion.cpp
+++ recursion.cpp
@@ -8,3 +8,14 @@ class Solution {
         cout<<n<<endl;
     }
 };
+
+class Solution {
+  public:
+    void printNumbers(int n) {
+        if(n==0){
+            return;
+        }
+        cout<<n<<endl;
+        printNumbers(n-1);
+    }
+};
