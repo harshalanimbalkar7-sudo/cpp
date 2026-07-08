d792a7a073f04ff6012a04a00aeaff476a99f75c
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-07-08 16:36:49 +0530

    Add files via upload

--- function.cpp
+++ function.cpp
@@ -1,15 +1,31 @@
+// #include <iostream>
+// using namespace std;
+
+// int sum(int a, int b)
+// {
+//     return a + b;
+// }
+
+// int main()
+// {
+//     int a, b;
+//     cin >> a >> b;
+//     cout << "Sum = " << sum(a, b);
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
 
-int sum(int a, int b)
+int cube(int n)
 {
-    return a + b;
+    return n * n * n;
 }
 
 int main()
 {
-    int a, b;
-    cin >> a >> b;
-    cout << "Sum = " << sum(a, b);
+    int n;
+    cin >> n;
+    cout << cube(n);
     return 0;
 }
