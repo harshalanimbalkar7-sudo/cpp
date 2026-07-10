3a1de6b892dbefc20621ca39953d0cfd15109e30
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-07-10 14:52:54 +0530

    Add files via upload

--- sorting.cpp
+++ sorting.cpp
@@ -20,3 +20,28 @@ int n = nums.size();
         return nums;
     }
 };
+
+
+class Solution {
+public:
+    vector<int> bubbleSort(vector<int>& nums) {
+int n = nums.size();
+
+        for (int i = 0; i < n - 1; i++) {
+            bool swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    swap(nums[j], nums[j + 1]);
+                    swapped = true;
+                }
+            }
+
+            // If no swapping occurred, array is already sorted
+            if (!swapped)
+                break;
+        }
+
+        return nums;
+    }
+};
