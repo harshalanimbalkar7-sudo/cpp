edb9190d0537e529cad717ac49f0aa593697a042
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-07-15 17:51:49 +0530

    Add files via upload

--- arrayQ.cpp
+++ arrayQ.cpp
@@ -11,3 +11,26 @@ for(int i=1;i<nums.size();i++){
 return largest;
     }
 };
+
+
+
+class Solution {
+public:
+    int secondLargestElement(vector<int>& nums) {
+        int largest = nums[0];
+        int secondLargest = -1;
+
+        for (int i = 1; i < nums.size(); i++) {
+
+            if (nums[i] > largest) {
+                secondLargest = largest;
+                largest = nums[i];
+            }
+            else if (nums[i] < largest && nums[i] > secondLargest) {
+                secondLargest = nums[i];
+            }
+        }
+
+        return secondLargest;
+    }
+};
