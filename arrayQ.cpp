47ab12e5ccfb9148498c49b5fc9c9a660db8f936
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-07-15 10:32:20 +0530

    Add files via upload

---
+++ arrayQ.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int largestElement(vector<int>& nums) {
+int largest=nums[0];
+
+for(int i=1;i<nums.size();i++){
+    if(nums[i]>largest){
+        largest=nums[i];
+    }
+}
+return largest;
+    }
+};
