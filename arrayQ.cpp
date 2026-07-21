94485d2921fa5e34a91d0129cbc78177a17fa92b
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-07-21 22:54:47 +0530

    Add files via upload

--- arrayQ.cpp
+++ arrayQ.cpp
@@ -51,3 +51,34 @@ public:
         return true;
     }
 };
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        k = k % n;
+
+        reverse(nums.begin(), nums.end());
+
+        reverse(nums.begin(), nums.begin() + k);
+
+        reverse(nums.begin() + k, nums.end());
+    }
+};
+
+
+
+class Solution {
+public:
+    int linearSearch(vector<int>& nums, int target) {
+for (int i = 0; i < nums.size(); i++) {
+
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
