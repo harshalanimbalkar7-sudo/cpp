e7bf51e02be8edb7ebc531d363efb4b73151de7e
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-07-23 20:43:51 +0530

    Add files via upload

--- practice.cpp
+++ practice.cpp
@@ -87,3 +87,29 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums, int k) {
+
+        unordered_map<int, int> mp;
+        int sum = 0, maxLen = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            sum += nums[i];
+
+            if (sum == k)
+                maxLen = i + 1;
+
+            if (mp.find(sum - k) != mp.end())
+                maxLen = max(maxLen, i - mp[sum - k]);
+
+            if (mp.find(sum) == mp.end())
+                mp[sum] = i;
+        }
+
+        return maxLen;
+    }
+};
