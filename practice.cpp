d18e46620d0660d86efdf45233d9d497aba61ee9
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-07-23 18:57:09 +0530

    Add files via upload

--- practice.cpp
+++ practice.cpp
@@ -72,3 +72,18 @@ public:
         return maxCount;
     }
 };
+
+
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            ans = ans ^ nums[i];
+        }
+
+        return ans;
+    }
+};
