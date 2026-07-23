706c9cfb4903399f10e591857b7b8f86c467f8e5
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-07-23 20:58:24 +0530

    Add files via upload

--- basiccpp.cpp
+++ basiccpp.cpp
@@ -91,3 +91,18 @@ public:
         return sum;
     }
 };
+
+class Solution {
+public:
+    void reverseArray(vector<int>& arr) {
+
+        int left = 0;
+        int right = arr.size() - 1;
+
+        while (left < right) {
+            swap(arr[left], arr[right]);
+            left++;
+            right--;
+        }
+    }
+};
