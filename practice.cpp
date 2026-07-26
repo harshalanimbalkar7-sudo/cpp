2c7e3a0354327e285126085091d6fd5d7201ad7d
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-07-26 15:18:00 +0530

    Add files via upload

--- practice.cpp
+++ practice.cpp
@@ -163,3 +163,33 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+
+        unordered_set<int> st(nums.begin(), nums.end());
+
+        int longest = 0;
+
+        for (int num : st) {
+
+            // Check if num is the start of a sequence
+            if (st.find(num - 1) == st.end()) {
+
+                int current = num;
+                int count = 1;
+
+                while (st.find(current + 1) != st.end()) {
+                    current++;
+                    count++;
+                }
+
+                longest = max(longest, count);
+            }
+        }
+
+        return longest;
+    }
+};
