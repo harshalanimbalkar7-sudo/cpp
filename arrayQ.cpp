44c6a91465a4609a0fac8e6bb78e0e60fd615f58
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-08-06 19:07:01 +0530

    Add files via upload

--- arrayQ.cpp
+++ arrayQ.cpp
@@ -82,3 +82,32 @@ for (int i = 0; i < nums.size(); i++) {
         return -1;
     }
 };
+
+
+class Solution {
+public:
+    int maxLen(vector<int>& arr) {
+
+        unordered_map<int, int> mp;
+
+        int sum = 0;
+        int maxi = 0;
+
+        for (int i = 0; i < arr.size(); i++) {
+
+            sum += arr[i];
+
+            if (sum == 0)
+                maxi = i + 1;
+
+            if (mp.find(sum) != mp.end()) {
+                maxi = max(maxi, i - mp[sum]);
+            }
+            else {
+                mp[sum] = i;
+            }
+        }
+
+        return maxi;
+    }
+};
