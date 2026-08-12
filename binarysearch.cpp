a8d4b68a75175720f5c02e37cbf5ced98b0b0e08
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-08-12 17:43:28 +0530

    Add files via upload

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -22,3 +22,30 @@ public:
         return ans;
     }
 };
+
+
+//12-8-26
+class Solution {
+public:
+    int upperBound(vector<int>& nums, int x) {
+
+        int low = 0;
+        int high = nums.size() - 1;
+        int ans = nums.size();
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] > x) {
+                ans = mid;
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+};
