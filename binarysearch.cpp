d5666efafe6942cdf4001c34ad97fc1e621a9b93
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-08-12 18:54:44 +0530

    Add files via upload

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -49,3 +49,40 @@ public:
         return ans;
     }
 };
+
+
+
+class Solution {
+public:
+    vector<int> getFloorAndCeil(vector<int>& nums, int x) {
+
+        int low = 0;
+        int high = nums.size() - 1;
+
+        int floor = -1;
+        int ceil = -1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == x) {
+                floor = nums[mid];
+                ceil = nums[mid];
+                break;
+            }
+
+            else if (nums[mid] < x) {
+                floor = nums[mid];
+                low = mid + 1;
+            }
+
+            else {
+                ceil = nums[mid];
+                high = mid - 1;
+            }
+        }
+
+        return {floor, ceil};
+    }
+};
