078546d486ca38f960b7e6e77026ddded30c4749
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-08-13 18:10:25 +0530

    Add files via upload

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -86,3 +86,55 @@ public:
         return {floor, ceil};
     }
 };
+
+//13-08-26
+
+class Solution {
+public:
+    int countOccurrences(vector<int>& arr, int target) {
+
+        int low = 0, high = arr.size() - 1;
+        int first = -1, last = -1;
+
+        // Find first occurrence
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                first = mid;
+                high = mid - 1;
+            }
+            else if (arr[mid] < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+
+        // Target not found
+        if (first == -1)
+            return 0;
+
+        low = 0;
+        high = arr.size() - 1;
+
+        // Find last occurrence
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                last = mid;
+                low = mid + 1;
+            }
+            else if (arr[mid] < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return last - first + 1;
+    }
+};
