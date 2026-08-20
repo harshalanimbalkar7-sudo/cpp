1497438dc106b6febceba63c04a6aa0320cb680f
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-08-20 19:24:08 +0530

    Add files via upload

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -247,3 +247,57 @@ public:
         return -1;
     }
 };
+
+//20-8-26
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    bool canPlace(vector<int>& stalls, int k, int distance)
+    {
+        int cows = 1;
+        int lastPosition = stalls[0];
+
+        for(int i = 1; i < stalls.size(); i++)
+        {
+            if(stalls[i] - lastPosition >= distance)
+            {
+                cows++;
+                lastPosition = stalls[i];
+            }
+
+            if(cows >= k)
+                return true;
+        }
+
+        return false;
+    }
+
+    int aggressiveCows(vector<int>& stalls, int k)
+    {
+        sort(stalls.begin(), stalls.end());
+
+        int low = 0;
+        int high = stalls.back() - stalls.front();
+        int answer = 0;
+
+        while(low <= high)
+        {
+            int mid = low + (high - low) / 2;
+
+            if(canPlace(stalls, k, mid))
+            {
+                answer = mid;
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+
+        return answer;
+    }
+};
