7a1a7faba60555cff5d430809086f2e245850832
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-08-28 20:49:21 +0530

    Add files via upload

--- string.cpp
+++ string.cpp
@@ -15,3 +15,28 @@ bool isPalindrome(string s) {
 
     return true;
 }
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isAnagram(string s, string t) {
+
+    if (s.length() != t.length())
+        return false;
+
+    int freq[26] = {0};
+
+    for (char ch : s)
+        freq[ch - 'a']++;
+
+    for (char ch : t)
+        freq[ch - 'a']--;
+
+    for (int i = 0; i < 26; i++) {
+        if (freq[i] != 0)
+            return false;
+    }
+
+    return true;
+}
