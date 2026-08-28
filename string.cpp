035947285053c3540519fa32a5e2bde8bed9277b
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-08-28 20:50:19 +0530

    Add files via upload

--- string.cpp
+++ string.cpp
@@ -40,3 +40,22 @@ bool isAnagram(string s, string t) {
 
     return true;
 }
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int firstUniqChar(string s) {
+
+    int freq[26] = {0};
+
+    for (char ch : s)
+        freq[ch - 'a']++;
+
+    for (int i = 0; i < s.length(); i++) {
+        if (freq[s[i] - 'a'] == 1)
+            return i;
+    }
+
+    return -1;
+}
