1b93d7fbac21318e58378cd51277a74c8dc44ef6
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-09-02 15:34:58 +0530

    Add files via upload

--- string.cpp
+++ string.cpp
@@ -59,3 +59,25 @@ int firstUniqChar(string s) {
 
     return -1;
 }
+
+
+//02-09-26
+class Solution {
+public:
+    int maxDepth(string s) {
+        int depth = 0;
+        int maxDepth = 0;
+
+        for (char ch : s) {
+            if (ch == '(') {
+                depth++;
+                maxDepth = max(maxDepth, depth);
+            }
+            else if (ch == ')') {
+                depth--;
+            }
+        }
+
+        return maxDepth;
+    }
+};
