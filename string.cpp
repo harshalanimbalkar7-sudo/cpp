1f219e483730dc6205a185b5402aae1c9b5133b2
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-09-04 21:38:05 +0530

    Add files via upload

--- string.cpp
+++ string.cpp
@@ -81,3 +81,68 @@ public:
         return maxDepth;
     }
 };
+
+class Solution {
+public:
+    string frequencySort(string s) {
+
+        // Step 1: Count frequency of every character
+        unordered_map<char, int> freq;
+
+        for (char ch : s) {
+            freq[ch]++;
+        }
+
+        // Step 2: Store {character, frequency}
+        vector<pair<char, int>> arr;
+
+        for (auto it : freq) {
+            arr.push_back({it.first, it.second});
+        }
+
+        // Step 3: Sort by frequency in decreasing order
+        sort(arr.begin(), arr.end(), [](auto &a, auto &b) {
+            return a.second > b.second;
+        });
+
+        // Step 4: Build answer
+        string ans = "";
+
+        for (auto &p : arr) {
+
+            char ch = p.first;
+            int count = p.second;
+
+            while (count--) {
+                ans += ch;
+            }
+        }
+
+        return ans;
+    }
+};
+
+//04-09-26
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isAnagram(string s, string t) {
+    if (s.length() != t.length())
+        return false;
+
+    int freq[26] = {0};
+
+    for (char c : s)
+        freq[c - 'a']++;
+
+    for (char c : t)
+        freq[c - 'a']--;
+
+    for (int x : freq) {
+        if (x != 0)
+            return false;
+    }
+
+    return true;
+}
