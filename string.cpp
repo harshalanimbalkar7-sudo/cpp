4c089c573bb6dc9c85fe0f932e7bce7640aaa652
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-09-06 23:50:44 +0530

    Add files via upload

--- string.cpp
+++ string.cpp
@@ -527,3 +527,34 @@ string intToRoman(int num) {
 
     return ans;
 }
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isValid(string s) {
+
+    stack<char> st;
+
+    for (char c : s) {
+
+        if (c == '(' || c == '{' || c == '[') {
+            st.push(c);
+        }
+        else {
+
+            if (st.empty())
+                return false;
+
+            char top = st.top();
+            st.pop();
+
+            if ((c == ')' && top != '(') ||
+                (c == '}' && top != '{') ||
+                (c == ']' && top != '[')) {
+                return false;
+            }
+        }
+    }
+
+    return st.empty();
+}
