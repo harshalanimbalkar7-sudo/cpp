82f8be4fd619786dc88f96dad4bc6d4f484e698a
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-09-08 21:12:55 +0530

    Add files via upload

--- string2.cpp
+++ string2.cpp
@@ -95,3 +95,51 @@ vector<int> findAnagrams(string s, string p) {
 
     return ans;
 }
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string minWindow(string s, string t) {
+
+    if (t.length() > s.length())
+        return "";
+
+    int freq[256] = {0};
+
+    for (char c : t)
+        freq[c]++;
+
+    int left = 0;
+    int count = t.length();
+
+    int start = 0;
+    int minLen = INT_MAX;
+
+    for (int right = 0; right < s.length(); right++) {
+
+        if (freq[s[right]] > 0)
+            count--;
+
+        freq[s[right]]--;
+
+        while (count == 0) {
+
+            if (right - left + 1 < minLen) {
+                minLen = right - left + 1;
+                start = left;
+            }
+
+            freq[s[left]]++;
+
+            if (freq[s[left]] > 0)
+                count++;
+
+            left++;
+        }
+    }
+
+    if (minLen == INT_MAX)
+        return "";
+
+    return s.substr(start, minLen);
+}
