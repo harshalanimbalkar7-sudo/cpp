a2be5b7d41ff0521b99558a63bba7b0a3a2132e6
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-09-08 21:10:30 +0530

    Add files via upload

---
+++ string2.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string reverseWords(string s) {
+
+    stringstream ss(s);
+
+    vector<string> words;
+    string word;
+
+    while (ss >> word)
+        words.push_back(word);
+
+    reverse(words.begin(), words.end());
+
+    string ans;
+
+    for (int i = 0; i < words.size(); i++) {
+
+        if (i > 0)
+            ans += " ";
+
+        ans += words[i];
+    }
+
+    return ans;
+}
