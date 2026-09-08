0f6aee1321e234b99c7f6dcb576576432ebc5ee9
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-09-08 21:12:09 +0530

    Add files via upload

--- string2.cpp
+++ string2.cpp
@@ -51,3 +51,47 @@ int lengthOfLongestSubstring(string s) {
 
     return ans;
 }
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> findAnagrams(string s, string p) {
+
+    vector<int> ans;
+
+    if (p.length() > s.length())
+        return ans;
+
+    int need[26] = {0};
+    int window[26] = {0};
+
+    for (char c : p)
+        need[c - 'a']++;
+
+    int k = p.length();
+
+    for (int i = 0; i < s.length(); i++) {
+
+        window[s[i] - 'a']++;
+
+        if (i >= k)
+            window[s[i - k] - 'a']--;
+
+        if (i >= k - 1) {
+
+            bool same = true;
+
+            for (int j = 0; j < 26; j++) {
+                if (need[j] != window[j]) {
+                    same = false;
+                    break;
+                }
+            }
+
+            if (same)
+                ans.push_back(i - k + 1);
+        }
+    }
+
+    return ans;
+}
