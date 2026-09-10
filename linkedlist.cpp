8b9db45646ebd2bb1f29a20800fa582ba013c4fb
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-09-10 18:46:43 +0530

    Add files via upload

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -14,3 +14,17 @@ public:
         return head;
     }
 };
+
+class Solution {
+public:
+    int getCount(ListNode* head) {
+        int count = 0;
+
+        while (head != NULL) {
+            count++;
+            head = head->next;
+        }
+
+        return count;
+    }
+};
