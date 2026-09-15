2fd40982e4cf9c2ab770059ef497fa93bd5af4cd
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-09-15 15:48:41 +0530

    Add files via upload

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -112,3 +112,37 @@ public:
         return head;
     }
 };
+
+
+//15-09-26
+class Solution {
+public:
+    int lengthOfLoop(Node* head) {
+        Node* slow = head;
+        Node* fast = head;
+
+        // Step 1: Detect the loop
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            // Loop found
+            if (slow == fast) {
+                
+                // Step 2: Count the nodes in the loop
+                int count = 1;
+                Node* temp = slow->next;
+
+                while (temp != slow) {
+                    count++;
+                    temp = temp->next;
+                }
+
+                return count;
+            }
+        }
+
+        // No loop
+        return 0;
+    }
+};
