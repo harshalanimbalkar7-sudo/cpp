0a830aff50982f90b798ec1c7589f415c9ed8ee2
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-09-19 21:32:16 +0530

    Add files via upload

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -326,3 +326,42 @@ public:
         return head;
     }
 };
+
+//19-09-26
+class Solution {
+public:
+    vector<vector<int>> findPairsWithGivenSum(ListNode* head, int target) {
+        vector<vector<int>> ans;
+
+        if (head == NULL)
+            return ans;
+
+        // Find the last node
+        ListNode* right = head;
+        while (right->next != NULL) {
+            right = right->next;
+        }
+
+        ListNode* left = head;
+
+        while (left != NULL && right != NULL && left != right && left->prev != right) {
+            
+            int sum = left->val + right->val;
+
+            if (sum == target) {
+                ans.push_back({left->val, right->val});
+
+                left = left->next;
+                right = right->prev;
+            }
+            else if (sum < target) {
+                left = left->next;
+            }
+            else {
+                right = right->prev;
+            }
+        }
+
+        return ans;
+    }
+};
